08eb116898a893e732c6feceefbb6bdf24086209
Yash Mishra <praisedboy5000@gmail.com>
2024-04-24 16:56:36 +0530

    corections

--- components/Homepage/Homepage.js
+++ components/Homepage/Homepage.js
@@ -59,7 +59,7 @@ export default function Homepage() {
 
             e.preventDefault();
             setLoading(true);
-            const response = await axios.post("https://quiz-app-yl47.onrenpder.com/auth/otp/", formData);
+            const response = await axios.post("https://quiz-app-yl47.onrender.com/auth/otp/", formData);
             // console.log(formData);
             let Email = formData.email;
             let student = formData.student_no;
